ceb551691d3b195195d941e80b0283ba293f2a5a
luizarsantana <luiza.santana@sou.inteli.edu.br>
2023-02-15 08:11:29 -0300

    atividade2

---
+++ SEMANA_03/02_ATIVIDADES/programação/atividade2.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var lista1 = [1,2,3]
+var insira_lista = []
+var texto1 = ('bananas são amarelas')
+var insira_texto
+
+
+func _on_ListaPreenchida_pressed():
+	$lista1.text = "A LISTA É " + String(lista1)
+	
+
+func _on_ListaVazia_pressed():
+	insira_lista.append($insira_lista.text)
+	$lista2.text = "SUA LISTA É " + String(insira_lista)
+
+
+func _on_TextoPronto_pressed():
+	$texto1.text = "O TEXTO É " + String(texto1)
+	
+	
+func _on_TextoInserido_pressed():
+	insira_texto = str($insira_texto.text)
+	$texto2.text = "SEU TEXTO É " + String(insira_texto)
